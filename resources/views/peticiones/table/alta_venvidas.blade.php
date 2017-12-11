92653e99c2b2db3c2ba0b5ffeb1ab9c86b69f582
wilfr <wilfr@DESKTOP-5617O4K>
2017-12-11 17:07:20 -0400

    trabajando con las peticiones

---
+++ resources/views/peticiones/table/alta_venvidas.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilfr
+ * Date: 11/12/2017
+ * Time: 4:31 PM
+ */
---
+++ resources/views/peticiones/table/atendidas.blade.php
@@ -0,0 +1,33 @@
+<div class="card">
+    <div class="card-header" data-background-color="red">
+        <h4 class="title">EN ALTAS Y VENCIDAS</h4>
+        <p class="category">Lista de solicitudes </p>
+    </div>
+    <div class="card-content table-responsive">
+        <table class="table table-hover">
+            <thead class="text-warning">
+            <tr>
+                <th>ID</th>
+                <th>Fecha Reg.</th>
+                <th>Fecha Compromiso</th>
+                <th>Sub Cat.</th>
+                <th>Usuario</th>
+                <th>Descripcion</th>
+                <th>imagen</th>
+                <th>Accion</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Dakota Rice</td>
+                <td>$36,738</td>
+                <td>Niger</td>
+                <td>Niger</td>
+                <td>Niger</td>
+                <td>Niger</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
